9d2b82aa2603647a071353525150ced7ef0a5590
Gianluca La Rosa <gianluca_larosa@hotmail.com>
2022-04-15 12:54:45 +0200

    WIP carousel

--- src/components/ui/Carousel.jsx
+++ src/components/ui/Carousel.jsx
@@ -10,33 +10,49 @@ function Carousel() {
     <>
       <div className={classes.carousel}>
         <IconContext.Provider value={{ className: classes.react_icons }}>
-          <div>
-            <IoConstructOutline />
-            <p>Falegnameria</p>
-          </div>
-          <div>
-            <SiArduino />
-            <p>Elettronica</p>
-          </div>
-          <div>
-            <FaTheaterMasks />
-            <p>Teatro</p>
-          </div>
-          <div>
-            <IoBeerOutline />
-            <p>Birra artigianale</p>
-          </div>
-          <div>
-            <FaGlobeAmericas />
-            <p>Lingue</p>
-          </div>
-          <div>
+          <div className={classes.cook}>
             <GiCook />
             <p>Cucina</p>
           </div>
-          <div>
+          <div className={classes.chess}>
             <FaChessPawn />
             <p>Giochi da tavolo</p>
+          </div>
+          <div className={classes.construct}>
+            <IoConstructOutline />
+            <p>Falegnameria</p>
+          </div>
+          <div className={classes.arduino}>
+            <SiArduino />
+            <p>Elettronica</p>
+          </div>
+          <div className={classes.theater}>
+            <FaTheaterMasks />
+            <p>Teatro</p>
+          </div>
+          <div className={classes.beer}>
+            <IoBeerOutline />
+            <p>Birra artigianale</p>
+          </div>
+          <div className={classes.globe}>
+            <FaGlobeAmericas />
+            <p>Lingue</p>
+          </div>
+          <div className={classes.cooks}>
+            <GiCook />
+            <p>Cucina</p>
+          </div>
+          <div className={classes.chesss}>
+            <FaChessPawn />
+            <p>Giochi da tavolo</p>
+          </div>
+          <div className={classes.constructs}>
+            <IoConstructOutline />
+            <p>Falegnameria</p>
+          </div>
+          <div className={classes.arduinos}>
+            <SiArduino />
+            <p>Elettronica</p>
           </div>
         </IconContext.Provider>
       </div>
